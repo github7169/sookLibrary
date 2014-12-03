a589448c9ce015db5043446dc24aac4d5daf303e
Jinhee <Jinhee@Jinhee-PC>
2014-12-03 18:55:58 +0900

    add getUser class

--- LIS/src/com/sook/DAO/UsersDAO.java
+++ LIS/src/com/sook/DAO/UsersDAO.java
@@ -20,18 +20,19 @@ public class UsersDAO {
 	private final String INSERT_USER = "INSERT INTO users VALUE(?,?,?,?,?);";
 	private final String UPDATE_USER = "UPDATE user SET(?,?,?) where id=?;";
 		//학생의 아이디 혹은 이름으로 검색 
-	private final String GET_USERS_ID = "SELECT userId,userName,userDepartment,userPhoneNum,userStatus "
+	private final String GET_USERS_ID = "SELECT * "
 			+ "FROM users WHERE userId LIKE ?";
 	
-	private final String GET_USERS_NAME = "SELECT userId,userName,userDepartment,userPhoneNum,userStatus "
+	private final String GET_USERS_NAME = "SELECT * "
 			+ "FROM users WHERE userName LIKE ?";
+	
 	//학생의 상태에 따른 (대출가능,연체중,대출제한) 검색
-	private final String GET_USERS_STATUS = "SELECT userId,userName,userDepartment,userPhoneNum,userStatus "
-			+ "FROM users WHERE userStatus=?;";
+	private final String GET_USERS_STATUS = "SELECT * "
+			+ "FROM users WHERE userStatus LIKE ?";
 	
-	
-	
-	
+	private final String GET_LENTED_LIST = "SELECT * FROM books "
+			+ "WHERE bookRentedBy = ?;";
+
 	private java.sql.Connection conn =null;
 	private java.sql.PreparedStatement pstmt=null;
 	private ResultSet rs =null;
@@ -93,13 +94,18 @@ public class UsersDAO {
 
 	//(Librarian) - 회원 조회 (getusers.jsp)
 	public ArrayList<UsersDTO> getUsers(UsersDTO usersDTO, int option, String keyword){
+		
+		
+		
+		//option = 20;
+		//keyword = "jinhee";	
+		
+		conn = JDBCUtil.getInstance().getConnection();
 		ArrayList<UsersDTO> list = new ArrayList<UsersDTO>();
 		
-		option = 19;
-		keyword = "진희";	
-		
 		try {
-
+			System.out.println( "나와랏" );
+			
 			if ( option == StatusUtil.userOptionId ){				
 				pstmt = conn.prepareStatement(GET_USERS_ID);
 
@@ -112,12 +118,12 @@ public class UsersDAO {
 			} else if ( option == StatusUtil.userStatusRestricted ){				
 				pstmt = conn.prepareStatement(GET_USERS_STATUS);
 				
-			} else if ( option == StatusUtil.userStatusOverdue ){				
-				pstmt = conn.prepareStatement(GET_USERS_STATUS);
-				
-			} else if ( option == StatusUtil.userStatusAvailable){				
-				pstmt = conn.prepareStatement(GET_USERS_STATUS);
-				
+			} else if ( option == StatusUtil.userStatusAvailable ){				
+			pstmt = conn.prepareStatement(GET_USERS_STATUS);
+			}
+			
+			else if ( option == StatusUtil.userStatusOverdue ){				
+			pstmt = conn.prepareStatement(GET_USERS_STATUS);
 			}
 			
 			pstmt.setString(1, "%" + keyword + "%");
@@ -130,15 +136,26 @@ public class UsersDAO {
 				
 				users.setUserId(rs.getString("userId"));
 				users.setUserName(rs.getString("userName"));
+				users.setUserPhoneNum(rs.getString("userPhoneNum"));
+				users.setUserDepartment(rs.getString("userDepartment"));
 				users.setUserStatus(rs.getInt("userStatus"));
+				
 				list.add(users);
 			}
 				
-			//test
 			for(int i=0; i<list.size() ; i++){
-				System.out.println( "list얌 "+ list.get(i).getUserId());
+				System.out.println("루프를 돕니다.");
+				System.out.println("유저네임!"+ list.get(i).getUserName() + ", 유저 아이디!"+ list.get(i).getUserId() );
+				System.out.println("유저 학과!"+ list.get(i).getUserDepartment() + ", 유저 폰넘버! " + list.get(i).getUserPhoneNum());
+				System.out.println("-----------------------------------------------------------");
+			int status;
+				status=list.get(i).getUserStatus();
+				if (status==4){
+					System.out.println("연체중입니다.   " + list.get(i).getUserStatus());
+				}
 			}
 			
+
 		} catch (SQLException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();

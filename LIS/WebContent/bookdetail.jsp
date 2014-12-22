4c26c5efaaf029efda5c5a3184eca5e91b1cb25b
Jisu <Jisu@Jisu-PC>
2014-12-23 01:42:34 +0900

    죄송해여여러분...

---
+++ LIS/WebContent/bookdetail.jsp
@@ -0,0 +1,117 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+<script type="text/javascript">
+	function deleteBook(){
+		var status = confirm("정말로 삭제하시겠습니까?");
+		if(status==true)
+			window.location.href='<c:url value="books/deleteBook"/>';
+	}
+</script>
+</head>
+<body>
+<form action="books/insertBook" method="post">
+	
+	<c:if test="${USER.userPosition == 'librarian'}">	
+	<c:forEach var="booklist" items="${GETBOOKLIST}">		
+	<table>
+			<tr>
+				<td>등록번호</td><td> <input type="text" name="bookRegistNumber" value="${booklist.bookRegistNumber}"> </td>
+				<td> ISBN</td><td> <input type="text" name="bookISBN" value="${booklist.bookISBN}"></td>
+				<td> 이용자 </td><td>${booklist.bookRentedBy.userId}</td>
+			</tr>
+			<tr>
+				<td>서명</td><td> <input type="text" name="bookTitle" value="${booklist.bookTitle}"> </td>
+				<td>청구기호</td><td> <input type="text" name="bookApplicationMark" value="${booklist.bookApplicationMark}"></td>
+				<td>대출상태 </td>
+				<c:if test="${booklist.bookStatus == '6'}"><td>대출가능</td></c:if> 
+				<c:if test="${booklist.bookStatus == '7'}"><td>대출중</td></c:if> 
+			</tr>
+			<tr>
+				<td>저자 </td><td><input type="text" name="bookAuthor" value="${booklist.bookAuthor}"></td>
+				<td>분류</td>
+				<td>
+					<input type="radio" name="bookCategory" value="8" <c:if test="${booklist.bookCategory == '8'}">
+					checked="checked" </c:if> >문학
+					<input type="radio" name="bookCategory" value="9" <c:if test="${booklist.bookCategory == '9'}">
+					checked="checked" </c:if> >역사
+					<input type="radio" name="bookCategory" value="10" <c:if test="${booklist.bookCategory == '10'}">
+					checked="checked" </c:if> >IT
+					<input type="radio" name="bookCategory" value="11" <c:if test="${booklist.bookCategory == '11'}">
+					checked="checked" </c:if> >예술
+					<input type="radio" name="bookCategory" value="12" <c:if test="${booklist.bookCategory == '12'}">
+					checked="checked" </c:if> >사회
+					<input type="radio" name="bookCategory" value="13" <c:if test="${booklist.bookCategory == '13'}">
+					checked="checked" </c:if> >기타
+				</td>
+				<td>대출날짜</td><td>${booklist.bookRentDate}</td>
+			</tr>
+			<tr>
+				<td>출판사</td><td><input type="text" name="bookPublisher" value="${booklist.bookPublisher}"></td>
+				<td>가격</td><td><input type="text" name="bookPrice" value="${booklist.bookPrice}"></td>
+				<td>반납예정일</td><td>${booklist.bookReturnDate}</td>
+			</tr>
+			<tr>
+				<td>출판년도</td><td> <input type="text" name="bookPublicationYear" value="${booklist.bookPublicationYear}"></td>
+				<td>대출횟수</td><td>${booklist.bookCount}</td>
+				<td></td>
+			</tr>
+		</table>
+		</c:forEach>
+		
+		<input type="button" value="수정" onclick="window.location.href='<c:url value="books/updateBook"/>'">
+		<input type="submit" value="저장">			
+		<input type="button" value="삭제" onClick="deleteBook()">
+	</c:if>
+	</form>
+	
+	<c:if test="${USER.userPosition == 'student'}">
+	<c:forEach var="booklist" items="${GETBOOKLIST}">	
+	<table>
+		<tr>
+			<td>등록번호</td><td>${booklist.bookRegistNumber}</td>
+			<td> ISBN</td><td>${booklist.bookISBN}</td>
+			<td>대출상태 </td>
+			<c:if test="${booklist.bookStatus == '6'}"><td>대출가능</td></c:if> 
+			<c:if test="${booklist.bookStatus == '7'}"><td>대출중</td></c:if> 				
+		</tr>
+		<tr>
+			<td>서명</td><td>${booklist.bookTitle}</td>
+			<td>청구기호</td><td>${booklist.bookApplicationMark}</td>
+			<td>대출날짜</td><td>${booklist.bookRentDate}</td>
+		</tr>
+		<tr>
+			<td>저자 </td><td>${booklist.bookAuthor}</td>
+			<td>분류</td>
+			<td>
+				<input type="radio" name="bookCategory" value="8" <c:if test="${booklist.bookCategory == '8'}">
+				checked="checked" </c:if> >문학
+				<input type="radio" name="bookCategory" value="9" <c:if test="${booklist.bookCategory == '9'}">
+				checked="checked" </c:if> >역사
+				<input type="radio" name="bookCategory" value="10" <c:if test="${booklist.bookCategory == '10'}">
+				checked="checked" </c:if> >IT
+				<input type="radio" name="bookCategory" value="11" <c:if test="${booklist.bookCategory == '11'}">
+				checked="checked" </c:if> >예술
+				<input type="radio" name="bookCategory" value="12" <c:if test="${booklist.bookCategory == '12'}">
+				checked="checked" </c:if> >사회
+				<input type="radio" name="bookCategory" value="13" <c:if test="${booklist.bookCategory == '13'}">
+				checked="checked" </c:if> >기타
+			</td>
+			<td>반납예정일</td><td>${booklist.bookReturnDate}</td>
+		</tr>
+		<tr>
+			<td>출판사</td><td>${booklist.bookPublisher}</td>
+		</tr>
+		<tr>
+			<td>출판년도</td><td>${booklist.bookPublicationYear}</td>
+		</tr>
+	</table>
+	</c:forEach>
+	</c:if>
+</body>
+</html>
---
+++ LIS/src/com/sook/controller/BooksController.java
@@ -0,0 +1,154 @@
+package com.sook.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sook.DAO.BooksDAO;
+import com.sook.DTO.BooksDTO;
+import com.sook.util.AbstractController;
+import com.sook.util.StatusUtil;
+
+public class BooksController extends AbstractController{
+	public void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String uri = getURI(request);
+		System.out.println("uri : "+uri);
+		if ("/books/getBooks".equals(uri)) {
+			selectBook(request, response);
+		} else if ("/books/getRentedList".equals(uri)) {
+			getRentedList(request, response);
+		} else if("/books/insertBook".equals(uri)) {
+			insertBook(request, response);
+		} else if("/books/deleteBook".equals(uri)) {
+			deleteBook(request, response);
+		} else if("/books/updateBook".equals(uri)){
+			updateBook(request, response);
+		}
+	}
+
+	private void updateBook(HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+		// TODO Auto-generated method stub
+		response.sendRedirect(request.getContextPath() + "/getbooks.jsp");
+	}
+
+	private void deleteBook(HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+		// TODO Auto-generated method stub
+		response.sendRedirect(request.getContextPath() + "/getbooks.jsp");
+	}
+
+	private void insertBook(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		BooksDTO booksDTO = new BooksDTO();
+		BooksDAO booksDAO = new BooksDAO();
+		
+		String bookRegistNumber = request.getParameter("bookRegistNumber"); 
+		String bookTitle = request.getParameter("bookTitle");
+		String bookAuthor = request.getParameter("bookAuthor");  	 
+		String bookPublisher = request.getParameter("bookPublisher");
+		int bookPublicationYear = Integer.parseInt(request.getParameter("bookPublicationYear")); 
+		String bookISBN = request.getParameter("bookISBN");
+		int bookApplicationMark = Integer.parseInt(request.getParameter("bookApplicationMark"));
+		int bookCategory = Integer.parseInt(request.getParameter("bookCategory"));
+		int bookPrice = Integer.parseInt(request.getParameter("bookPrice"));
+		
+		booksDTO.setBookApplicationMark(bookApplicationMark);
+		booksDTO.setBookAuthor(bookAuthor);
+		booksDTO.setBookCategory(bookCategory);
+		booksDTO.setBookISBN(bookISBN);
+		booksDTO.setBookPrice(bookPrice);
+		booksDTO.setBookPublicationYear(bookPublicationYear);
+		booksDTO.setBookPublisher(bookPublisher);
+		booksDTO.setBookRegistNumber(bookRegistNumber);
+		booksDTO.setBookTitle(bookTitle);
+		
+		try {
+			booksDAO.insertBook(booksDTO);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		response.sendRedirect(request.getContextPath() + "/getbooks.jsp");
+	}
+
+	private void getRentedList(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		
+		BooksDTO booksDTO = new BooksDTO();
+		BooksDAO booksDAO = new BooksDAO();			
+		System.out.println("rentedBook was called");
+		
+		String userId = request.getParameter("userId");
+		
+		ArrayList<BooksDTO> getRentedBookList = new ArrayList<BooksDTO>();
+				
+		getRentedBookList = booksDAO.getRentedList(booksDTO, userId);
+		if(getRentedBookList.size()==0){
+			request.setAttribute("notFound", "notFound");
+		}
+		request.setAttribute("GETRENTEDBOOKLIST", getRentedBookList );
+						
+		RequestDispatcher view = request.getRequestDispatcher("/getrentedlist.jsp");  
+		view.forward(request, response);  
+	}
+
+	private void selectBook(HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		
+			BooksDTO booksDTO = new BooksDTO();
+			BooksDAO booksDAO = new BooksDAO();			
+			System.out.println("selectBook was called");
+			
+			String keyword = request.getParameter("keyword");
+			String[] bookfilter = request.getParameterValues("book_filter");
+			
+			ArrayList<BooksDTO> getBookList =new ArrayList<BooksDTO>();
+			
+			int option = 0;
+			
+			switch (bookfilter[0]) {
+			case "registnum":
+				booksDTO.setBookRegistNumber(keyword);
+				option = StatusUtil.bookOptionRegistNumber;
+				break;
+			case "title":
+				booksDTO.setBookTitle(keyword);
+				option = StatusUtil.bookOptionTitle;
+				break;
+			case "author":
+				booksDTO.setBookAuthor(keyword);
+				option = StatusUtil.bookOptionAuthor;
+				break;
+			case "publisher":
+				booksDTO.setBookPublisher(keyword);
+				option = StatusUtil.bookOptionPublisher;
+				break;
+			case "ISBN":
+				booksDTO.setBookISBN(keyword);
+				option = StatusUtil.bookOptionISBN;
+				break;
+			default:
+				break;
+			}
+			getBookList = booksDAO.selectBook(booksDTO, option, keyword);
+
+			if(getBookList.size()==0){
+				request.setAttribute("notFound", "notFound");
+			}
+			request.setAttribute("GETBOOKLIST", getBookList );
+	
+	        RequestDispatcher rd = request.getRequestDispatcher("/getbooks.jsp");  
+	        rd.forward(request, response);  
+	}
+}
